7644b1e5bacb9a67875c9af6378a5ed4ad465fca
b i t O Ψ <32198261+bitopsy@users.noreply.github.com>
2023-10-12 17:53:25 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,1 @@
+# After my code has been used for commercial reasons without my consent, with no credits, I had to take it down. I did several updates to it and launched the latest version at https://plotgpt.fr if you want to check it out. A free trial is available.
