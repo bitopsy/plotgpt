0d99bf2c08dd4d03a2b6d11cb7fa07f17f0cd5ef
b i t O Ψ <32198261+bitopsy@users.noreply.github.com>
2023-11-19 12:00:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,3 @@
 # After my code has been used for commercial reasons without my consent, with no credits, I had to take it down. I did several updates to it and launched the latest version at https://plotgpt.fr if you want to check it out. A free trial is available.
+
+import pandas as pd
